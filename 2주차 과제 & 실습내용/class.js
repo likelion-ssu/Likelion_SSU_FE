fa4994f7b4e211882ab0f405f366e321694090c0
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2022-06-05 18:40:04 +0900

    Done!!

---
+++ 2주차 과제 & 실습내용/class.js
@@ -0,0 +1,18 @@
+// 클래스 생성
+//프론트 멤버
+
+//1. 클래스 정의 (이름, 번호)
+class Front {
+    constructor(name, number) {
+        this.name = name,
+        this,number= number,
+    }
+}
+
+//2. 클래스를 활용한 인스턴스
+
+const 준규 = new Front("준규", 1);
+
+
+
+console.log(준규)
